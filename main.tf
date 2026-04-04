68f8ecf3becf09aa07f60817a6adbe3857eea13a
ishu <ishusharma20032008@gmail.com>
2026-04-04 13:22:01 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,19 +1,21 @@
 terraform {
-required_providers {
-aws = {
-source = "hashicorp/aws"
-version = "~> 6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
-}
-}
+
 provider "aws" {
-region = "us-east-1"
-shared_credentials_files = ["./credentials"]
-default_tags {
-tags = {
-Environment = "Dev"
-Course = "CSSE6400"
-StudentID = "s4921211"
-}
-}
+  region                   = "us-east-1"
+  shared_credentials_files = ["./credentials"]
+
+  default_tags {
+    tags = {
+      Environment = "Dev"
+      Course      = "CSSE6400"
+      StudentID   = "s4921211"
+    }
+  }
 }
