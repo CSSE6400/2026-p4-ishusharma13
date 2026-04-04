6bdfebe315fa4ea647ae96fc5e38e273925e0011
ishu <ishusharma20032008@gmail.com>
2026-04-04 13:47:47 +0530

    init and plan

--- main.tf
+++ main.tf
@@ -10,58 +10,12 @@ terraform {
 provider "aws" {
   region                   = "us-east-1"
   shared_credentials_files = ["./credentials"]
+
   default_tags {
     tags = {
       Environment = "Dev"
       Course      = "CSSE6400"
       StudentID   = "s4921211"
     }
-  }
-}
-
-resource "aws_security_group" "hextris_sg" {
-  name        = "hextris-sg"
-  description = "Allow SSH and HTTP"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "hextris" {
-  ami                    = "ami-0c02fb55956c7d316"  # Replace if your lab specifies a different AMI
-  instance_type          = "t2.micro"
-  key_name               = "labsuser"
-  vpc_security_group_ids = [aws_security_group.hextris_sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y httpd git
-              sudo systemctl enable httpd
-              sudo systemctl start httpd
-              cd /var/www/html
-              sudo git clone https://github.com/Hextris/hextris .
-              EOF
-
-  tags = {
-    Name = "HextrisLab"
   }
 }
